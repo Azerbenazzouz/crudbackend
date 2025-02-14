357b242c8148f2a786e4e13825096779d3ef1f9a
Azer Ben Azzouz <benazzouzazer@gmail.com>
2025-02-14 22:12:24 +0100

    Ajout des colonnes 'response', 'user_id' et 'product_id' à la table 'generate_historique' avec des contraintes de clé étrangère.

--- database/migrations/2025_02_14_205833_create_generate_historique_table.php
+++ database/migrations/2025_02_14_205833_create_generate_historique_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('generate_historique', function (Blueprint $table) {
             $table->id();
             $table->string("prompt");
-            
+            $table->string("response");
+            $table->foreignId("user_id")->constrained()->onDelete("cascade");
+            $table->foreignId("product_id")->constrained()->onDelete("cascade");
             $table->timestamps();
         });
     }
